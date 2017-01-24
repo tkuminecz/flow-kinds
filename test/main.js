8350c812dd9b09bae8397843885d98939c76744f
Tim Kuminecz <tkuminecz@gmail.com>
2017-01-24 00:15:25 -0500

    save work

--- test/main.js
+++ test/main.js
@@ -1,14 +1,19 @@
 // @flow
-import { HKT, list1, fromT1 } from '../src/main';
+import type { $1List, $2List, $3List } from '../src/main';
+import { Higher, HKT, fromTuple, toTuple, list1, list2, list3 } from '../src/main';
 import test from 'tape';
 
 test('HKT', t => {
 	t.plan(1);
 
-	class A_Kind {}
+	class A_Kind extends Higher<$1List<number>> {}
 
 	let a = 42,
 		hkt_a = HKT.wrap(A_Kind, list1(a))
 
-	t.equal(fromT1(HKT.unwrap(A_Kind, hkt_a)), a);
+	t.equal(toTuple(HKT.unwrap(A_Kind, hkt_a)), a);
 });
+
+(list1('tim'): $1List<string>);
+(list2('tim', 29): $2List<string, string>);
+(list3('tim', 29, 32): $3List<string, number, bool>);
